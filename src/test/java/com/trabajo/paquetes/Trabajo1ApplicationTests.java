bd5eb555aba19cb0d98f07c895fde13943b186a6
FrancoHub25 <francoretamozo25@gmail.com>
2022-06-07 16:45:01 -0500

    primer

---
+++ src/test/java/com/trabajo/paquetes/Trabajo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabajo.paquetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabajo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
